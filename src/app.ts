bf34634f260be6de8f98268f1fc9cb814f89e7fa
Morgan Bazin <75011981+morganbazin@users.noreply.github.com>
2024-02-22 10:03:19 +0100

    script

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,10 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
 
 export default app;
